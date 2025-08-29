ae5a788e92c5dfba3d414149c107aae0f18897ed
Moaz Ahmed <justmeyo2025@gmail.com>
2025-08-29 22:41:11 +0300

    add: Read Config & Set User.

---
+++ internal/config/config.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DBUrl           string `json:"db_url"`
+	CurrentUsername string `json:"current_user_name"`
+}
+
+func Read() Config {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonCfg, err := os.ReadFile(configFilePath)
+	if err != nil {
+		log.Fatal("couldn't read the config file")
+	}
+
+	var config Config
+
+	json.Unmarshal(jsonCfg, &config)
+
+	return config
+}
+
+func SetUser(username string) {
+	if err := write(username); err != nil {
+		log.Fatal("couldn't write jsonConfig with user set")
+	}
+}
+
+func write(username string) error {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := Read()
+	config.CurrentUsername = username
+
+	jsonCfg, err := json.Marshal(&config)
+	if err != nil {
+		log.Fatal("couldn't set username.")
+	}
+
+	return os.WriteFile(configFilePath, jsonCfg, 0666)
+}
+
+func getConfigFilePath() (string, error) {
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", errors.New("couldn't get the user home directory")
+	}
+
+	jsonFilePath := fmt.Sprintf("%s/%s", userHomeDir, configFileName)
+	_, err = os.Stat(jsonFilePath)
+	if err != nil {
+		return "", errors.New("couldn't find the config in the user home directory")
+	}
+
+	return jsonFilePath, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/MeYo0o/blog_aggregator/internal/config"
+)
+
+func main() {
+	fmt.Println(config.Read())
+	config.SetUser("meyo")
+	fmt.Println(config.Read())
 }
